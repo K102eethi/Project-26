74a016dbc585da8acf659db986464a592b7a22f6
K102eethi <pallerlakeerthireddy222@gmail.com>
2025-10-14 17:18:32 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
+# Change ownership so ec2-user can write
+sudo chown -R ec2-user:ec2-user /home/ec2-user/nodejs-app
+
 # Navigate to app directory
 cd /home/ec2-user/nodejs-app
 
-# Update packages
-sudo yum update -y
-
-# Install Node.js and npm
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-sudo yum install -y nodejs
-
-# Install app dependencies
+# Install dependencies
 npm install
