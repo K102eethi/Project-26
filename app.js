d96c4c4b79c512f741882301847882397ae0bd71
K102eethi <pallerlakeerthireddy222@gmail.com>
2025-10-15 16:53:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Node.js App deployed via CodePipeline!,Thanks\n');
+  res.end('My Project completed successfully\n');
 });
 
 server.listen(port, () => {
